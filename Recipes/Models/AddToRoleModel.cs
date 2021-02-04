fa1312aa0dd4e4def412497d30e8abf740aaaafb
DimovNikola <nikoladimov45@yahoo.com>
2021-02-04 03:21:54 +0100

    initial, database, recipe list, user recipes and admin panel

---
+++ Recipes/Models/AddToRoleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recipes.Models
+{
+    public class AddToRoleModel
+    {
+        public string Email { get; set; }
+        public string SelectedRole { get; set; }
+        public List<String> Roles { get; set; }
+        public AddToRoleModel()
+        {
+            Roles = new List<string>();
+        }
+    }
+}
---
+++ Recipes/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Recipes.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Range(1, 5)]
+        public int Rating { get; set; }
+        [Required]
+        [Display(Name = "Image")]
+        public string ImageURL { get; set; }
+        public int UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
